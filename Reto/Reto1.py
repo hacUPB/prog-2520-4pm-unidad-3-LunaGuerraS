04fba83fc6d11e28a0b8936d7ad0bb4acd840082
Luna Guerra <luna.guerra@upb.edu.co>
2025-09-18 00:52:26 -0500

    Se mejoro el ejercicio 1 para hacerlo mas autonónomo

--- Reto/Reto1.py
+++ Reto/Reto1.py
@@ -2,20 +2,20 @@
 
 combustible = int(input("Porfa ingrese la cantidad inicial de combustible en litros: "))
 horas = int(input("Ingrese el el número de horas de vuelo: "))
-umbral_seguridad = combustible/1000
+consumo = int(input(f"Ingrese cuanto combustible se consume por hora: "))
+umbral_seguridad = combustible*0.25
 
 for hora in range(1, horas + 1):
-    consumo = int(input(f"Hora {hora}: Ingrese el consumo de combustible hasta ahora en litros: "))
-    combustible -= consumo
-    print(f"Después de la hora {hora}, quedan {combustible} litros de combustible.")
-    if combustible < umbral_seguridad:
-        print("Cuidado!! El combustible está por debajo del umbral de seguridad.")
-        emergencia = input("¿Quiere iniciar un aterrizaje de emergencia? (Si/No): ")
-        if emergencia() == "si":
-            print("Aterrizaje de emergencia iniciado.")
+        combustible -= consumo*hora
+        print(f"Después de la hora {hora}, quedan {combustible} litros de combustible.")
+        if combustible <= 0:
+            print("¡Emergencia! El avión se ha quedado sin combustible. Intente atterizaje de emerjencia en modo planeo.")
             break
-    if combustible <= 0:
-        print("¡Emergencia! El avión se ha quedado sin combustible. Intente atterizaje de emerjencia en modo planeo.")
-        break
-else:
-    print("Vuelo completado, Felicitaciones piloto :)")
+        if combustible < umbral_seguridad:
+            print("Cuidado!! El combustible está por debajo del umbral de seguridad.")
+            emergencia = input("¿Quiere iniciar un aterrizaje de emergencia? (Si/No): ")
+            if emergencia.upper() == "SI":
+                print("Aterrizaje de emergencia iniciado.")
+                break
+        else:
+            print("Vuelo completado, Felicitaciones piloto :D")
